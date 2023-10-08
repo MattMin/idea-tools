ca1deee4c8d649d455f8e376c5f1773f271fd39e
liziying <547507241@qq.com>
2023-10-08 16:52:18 +0800

    部分卦象2

---
+++ src/main/java/com/oeong/ui/fish/Divination.java
@@ -0,0 +1,9 @@
+package com.oeong.ui.fish;
+
+import javax.swing.*;
+
+public class Divination {
+    private JButton divinationButton;
+    private JTextPane 卦象风天小畜卦卦辞亨TextPane;
+    private JComboBox comboBox1;
+}
