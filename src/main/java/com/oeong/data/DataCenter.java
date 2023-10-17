e268ac9d1ecf560b4a3a6d5f242f19fe0d2c3d11
hongchenglong <oeong@foxmail.com>
2023-10-17 16:29:01 +0800

    feat: timing clock

---
+++ src/main/java/com/oeong/data/DataCenter.java
@@ -0,0 +1,30 @@
+package com.oeong.data;
+
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.regex.Pattern;
+
+public class DataCenter {
+    public static final Integer CLOSE = 0;
+    public static final Integer WORKING = 1;
+    public static final Integer RESTING = 2;
+    public static SettingData settingData;
+    public static Integer status = CLOSE;
+    public static LocalDateTime nextWorkTime;
+    public static LocalDateTime nextRestTime;
+    public static Timer workTimer = new Timer();
+    public static Timer reskTimer = new Timer();
+    public static int restCountDownSecond = -1;
+    public static final String RUNNING_TIP = "The work is running. (ง ˙o˙)ว";
+    public static final String STOPPED_TIP = "The work stopped. ʕ•̫͡• ʔ•̫͡•ཻʕ•̫͡•ʔ•͓͡•ʔ";
+    public static String WORK_TIME_TIP = "Let's get to work! °꒰๑'ꀾ'๑꒱°✨ The next rest time is %s. ";
+    public static final String REST_TIME_TIP = "After hard work, please have a rest. ฅ՞•ﻌ•՞ฅ";
+    public static final String WORK_TIP = "I want to keep working! ヽ( ຶ▮ ຶ)ﾉ!!!✨";
+    public static final String REST_TIP = "I need more rest. ˃̣̣̥᷄⌓˂̣̣̥᷅";
+    public static final String STOP_TIP = "I don't want to work. ꒦ິ^꒦ິ";
+
+    public static boolean isInteger(String str) {
+        Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+        return pattern.matcher(str).matches();
+    }
+}
--- src/main/java/com/oeong/ui/fish/TimingClock.java
+++ src/main/java/com/oeong/ui/fish/TimingClock.java
@@ -1,99 +1,77 @@
 package com.oeong.ui.fish;
 
+import com.oeong.service.TimerService;
+
 import javax.swing.*;
-import java.util.Timer;
-import java.util.TimerTask;
+
+import static com.oeong.data.DataCenter.RUNNING_TIP;
+import static com.oeong.data.DataCenter.STOPPED_TIP;
 
 // TODO: 2023/10/11 初始化异步调用请求，休息时间结束后还可以休息，语气活泼一点，下面的文字大一点
+// TODO: 2023/10/12 有些包不用
+
 public class TimingClock {
-    private JRadioButton startRadioButton;
-    private JRadioButton stopRadioButton;
-    private JTextField workField;
-    private JTextField restField;
+    public JRadioButton startRadioButton;
+    public JRadioButton stopRadioButton;
+    public JTextField workField;
+    public JTextField restField;
     private JPanel mainPanel;
-    private JLabel tipLabel;
-
-    private static final String startTip = "The work is running.";
-    private static final String stopTip = "The work hasn't started yet.";
-
+    private JLabel statusTipLabel;
+    public JLabel timeTipLabel;
 
     public TimingClock() {
-        String workFieldText = workField.getText();
-        String restFieldText = restField.getText();
 
-        Timer timer = new Timer(true);
-        WorkTimerTask workTimerTask = new WorkTimerTask();
+        statusTipLabel.setText(STOPPED_TIP);
 
         // status button
         startRadioButton.addActionListener(e -> {
             if (startRadioButton.isSelected()) {
-                stopRadioButton.setSelected(false);
-                startRadioButton.setText("Running");
-                stopRadioButton.setText("Stop");
-                tipLabel.setText(startTip);
-
-                JOptionPane.showConfirmDialog(null, "Start to work, please.", "Tip", JOptionPane.DEFAULT_OPTION);
-
-                long workMinutes = Long.parseLong(workFieldText);
-                long restMinutes = Long.parseLong(restFieldText);
-                timer.schedule(workTimerTask, restMinutes * 1000, workMinutes * 1000);
+                chooseStartButton();
+            } else {
+                chooseStopButton();
             }
+            handle(this);
         });
         stopRadioButton.addActionListener(e -> {
             if (stopRadioButton.isSelected()) {
-                startRadioButton.setSelected(false);
-                startRadioButton.setText("Start");
-                stopRadioButton.setText("Stopped");
-                tipLabel.setText(stopTip);
-
-                JOptionPane.showConfirmDialog(null, "Timing clock is stopped.", "Tip", JOptionPane.DEFAULT_OPTION);
-                timer.cancel();
+                chooseStopButton();
+            } else {
+                chooseStartButton();
             }
+            handle(this);
         });
-
-
     }
 
-    void verify() {
+    public void chooseStartButton() {
+        startRadioButton.setSelected(true);
+        stopRadioButton.setSelected(false);
+        startRadioButton.setText("Running");
+        stopRadioButton.setText("Stop");
+        statusTipLabel.setText(RUNNING_TIP);
+    }
+
+    public void chooseStopButton() {
+        stopRadioButton.setSelected(true);
+        startRadioButton.setSelected(false);
+        startRadioButton.setText("Start");
+        stopRadioButton.setText("Stopped");
+        statusTipLabel.setText(STOPPED_TIP);
+        timeTipLabel.setText("");
+    }
+
+    public void handle(TimingClock timingClock) {
+        TimerService.saveSetting(restField.getText(), workField.getText());
+
         if (startRadioButton.isSelected()) {
-            String workFieldText = workField.getText();
-            String restFieldText = restField.getText();
-
-            if (workFieldText == null || workFieldText.isEmpty()) {
-                JOptionPane.showMessageDialog(null, "请输入工作时间", "提示", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-            if (restFieldText == null || restFieldText.isEmpty()) {
-                JOptionPane.showMessageDialog(null, "请输入休息时间", "提示", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            int workMinutes = Integer.parseInt(workFieldText);
-            int restMinutes = Integer.parseInt(restFieldText);
-
-            if (workMinutes <= 0) {
-                JOptionPane.showMessageDialog(null, "工作时间必须大于0", "提示", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-            if (restMinutes <= 0) {
-                JOptionPane.showMessageDialog(null, "休息时间必须大于0", "提示", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            JOptionPane.showConfirmDialog(null, "开始工作", "提示", JOptionPane.DEFAULT_OPTION);
+            String notification = TimerService.openTimer(timingClock);
+            timeTipLabel.setText(notification);
+            JOptionPane.showMessageDialog(null, notification, "tips", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            TimerService.closeTimer();
         }
-
     }
 
     public JPanel getComponent() {
         return mainPanel;
-    }
-}
-
-class WorkTimerTask extends TimerTask {
-    @Override
-    public void run() {
-//        JOptionPane.showConfirmDialog(null, "Timing clock is running.", "Tip", JOptionPane.DEFAULT_OPTION);
-        System.out.println("Timing clock is running." + System.currentTimeMillis() / 1000);
     }
 }
