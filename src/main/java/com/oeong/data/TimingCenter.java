b28ec3fdf4540a57df21aa54b65af4468ea2074a
hongchenglong <oeong@foxmail.com>
2023-10-19 16:33:10 +0800

    style: set font size

--- src/main/java/com/oeong/data/TimingCenter.java
+++ src/main/java/com/oeong/data/TimingCenter.java
@@ -17,7 +17,7 @@ public class TimingCenter {
     public static final String RUNNING_TIP = "The work is running. (ง ˙o˙)ว";
     public static final String STOPPED_TIP = "The work stopped. ʕ•̫͡• ʔ•̫͡•ཻʕ•̫͡•ʔ•͓͡•ʔ";
     public static String WORK_TIME_TIP = "Let's get to work! °꒰๑'ꀾ'๑꒱°✨ The next rest time is %s. ";
-    public static final String REST_TIME_TIP = "After hard work, please have a rest. ฅ՞•ﻌ•՞ฅ";
+    public static final String REST_TIME_TIP = "It's rest time. ฅ՞•ﻌ•՞ฅ";
     public static final String WORK_TIP = "I want to keep working! ヽ( ຶ▮ ຶ)ﾉ!!!✨";
     public static final String REST_TIP = "I need more rest. ˃̣̣̥᷄⌓˂̣̣̥᷅";
     public static final String STOP_TIP = "I don't want to work. ꒦ິ^꒦ິ";
