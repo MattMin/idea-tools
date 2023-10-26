9ed3509505baf4571d1135c356b490d8d766e7a8
hongchenglong <oeong@foxmail.com>
2023-10-26 14:58:16 +0800

    fix: WORK_TIME_TIP

--- src/main/java/com/oeong/data/TimingCenter.java
+++ src/main/java/com/oeong/data/TimingCenter.java
@@ -16,7 +16,7 @@ public class TimingCenter {
     public static int restCountDownSecond = -1;
     public static final String RUNNING_TIP = "The work is running. (ง ˙o˙)ว";
     public static final String STOPPED_TIP = "The work stopped. ʕ•̫͡• ʔ•̫͡•ཻʕ•̫͡•ʔ•͓͡•ʔ";
-    public static String WORK_TIME_TIP = "Let's get to work! °꒰๑'ꀾ'๑꒱°✨ The next rest time is %s. ";
+    public static String WORK_TIME_TIP = "The work starts at %s and ends at %s.°꒰๑'ꀾ'๑꒱°✨";
     public static final String REST_TIME_TIP = "It's rest time. ฅ՞•ﻌ•՞ฅ";
     public static final String WORK_TIP = "I want to keep working! ヽ( ຶ▮ ຶ)ﾉ!!!✨";
     public static final String REST_TIP = "I need more rest. ˃̣̣̥᷄⌓˂̣̣̥᷅";
