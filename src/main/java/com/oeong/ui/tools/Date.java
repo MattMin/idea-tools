73c1af8e0cf2e1e922dd846b894ad5beff9b1588
hongchenglong <oeong@foxmail.com>
2023-11-20 14:21:58 +0800

    feat: date GUI

---
+++ src/main/java/com/oeong/ui/tools/Date.java
@@ -0,0 +1,22 @@
+package com.oeong.ui.tools;
+
+import javax.swing.*;
+
+public class Date {
+    private JTextField dateTextField1;
+    private JButton setTodayButton1;
+    private JButton setTodayButton2;
+    private JButton calculateButton1;
+    private JTextField answerTextField1;
+    private JTextField dateTextField3;
+    private JButton setTodayButton3;
+    private JComboBox compareComboBox;
+    private JTextField timeTextField;
+    private JComboBox unitComboBox;
+    private JButton calculateButton2;
+    private JTextField answerTextField2;
+    private JLabel date2;
+    private JLabel date1;
+    private JLabel gapLabel;
+    private JTextField dateTextField2;
+}
