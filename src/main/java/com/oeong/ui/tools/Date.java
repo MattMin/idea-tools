7c9ecaa3ed859c91eb574b949c56625950f5b856
hongchenglong <oeong@foxmail.com>
2023-11-21 10:18:44 +0800

    feat: date difference

--- src/main/java/com/oeong/ui/tools/Date.java
+++ src/main/java/com/oeong/ui/tools/Date.java
@@ -1,6 +1,12 @@
 package com.oeong.ui.tools;
 
+import com.oeong.notice.Notifier;
+import lombok.Getter;
+
 import javax.swing.*;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 
 public class Date {
     private JTextField dateTextField1;
@@ -15,8 +21,92 @@ public class Date {
     private JComboBox unitComboBox;
     private JButton calculateButton2;
     private JTextField answerTextField2;
-    private JLabel date2;
+    private JLabel date3;
     private JLabel date1;
-    private JLabel gapLabel;
+    private JLabel date2;
     private JTextField dateTextField2;
+    @Getter
+    private JPanel container;
+
+    public Date() {
+        // TODO: 2023/11/20 非空校验 
+        answerTextField1.setEditable(false);
+        answerTextField2.setEditable(false);
+
+        setTodayButton1.addActionListener(e -> dateTextField1.setText(getToday()));
+        setTodayButton2.addActionListener(e -> dateTextField2.setText(getToday()));
+        setTodayButton3.addActionListener(e -> dateTextField3.setText(getToday()));
+        calculateButton1.addActionListener(e -> {
+            String date1 = dateTextField1.getText();
+            String date2 = dateTextField2.getText();
+
+            LocalDate localDate1;
+            LocalDate localDate2;
+
+            try {
+                localDate1 = LocalDate.parse(date1);
+            } catch (Exception exception) {
+                Notifier.notifyError("First date error. Correct format: yyyy-MM-dd");
+                return;
+            }
+
+            try {
+                localDate2 = LocalDate.parse(date2);
+            } catch (Exception exception) {
+                Notifier.notifyError("Second date error. Correct format: yyyy-MM-dd");
+                return;
+            }
+
+            long daysDifference = ChronoUnit.DAYS.between(localDate1, localDate2);
+
+            answerTextField1.setText(String.valueOf(daysDifference));
+        });
+        calculateButton2.addActionListener(e -> {
+
+            String date = dateTextField3.getText();
+            String time = timeTextField.getText();
+            String compare = Objects.requireNonNull(compareComboBox.getSelectedItem()).toString();
+            String unit = Objects.requireNonNull(unitComboBox.getSelectedItem()).toString();
+
+            LocalDate localDate;
+            try {
+                localDate = LocalDate.parse(date);
+            } catch (Exception exception) {
+                Notifier.notifyError("Third date error. Correct format: yyyy-MM-dd");
+                return;
+            }
+
+            int timeInt;
+            try {
+                timeInt = Integer.parseInt(time);
+            } catch (Exception exception) {
+                Notifier.notifyError("Time error. You must enter an integer");
+                return;
+            }
+
+            answerTextField2.setText(calculateProjectedDate(localDate, timeInt, compare, unit));
+        });
+    }
+
+    public String getToday() {
+        LocalDate localDate = LocalDate.now();
+        return localDate.toString();
+    }
+
+    public String calculateProjectedDate(LocalDate date, int time, String compare, String unit) {
+
+        if (compare.equals("Before")) {
+            time = -time;
+        }
+
+        ChronoUnit chronoUnit = switch (unit) {
+            case "Days" -> ChronoUnit.DAYS;
+            case "Weeks" -> ChronoUnit.WEEKS;
+            case "Months" -> ChronoUnit.MONTHS;
+            default -> null;
+        };
+
+        assert chronoUnit != null;
+        return date.plus(time, chronoUnit).toString();
+    }
 }
